070a252f85b57b264a8b36ef844ad7a5198a9913
cookieshake <cookieshake@hotmail.com>
2017-07-04 13:16:01 +0900

    merge FillNNPLayer into ThreeGramLayer

--- src/main/scala/net/ingtra/hanalmot/layer/ThreeGramLayer.scala
+++ src/main/scala/net/ingtra/hanalmot/layer/ThreeGramLayer.scala
@@ -1,9 +1,13 @@
 package net.ingtra.hanalmot.layer
 
-import net.ingtra.hanalmot.component.{Candidates, Prediction}
+import net.ingtra.hanalmot.component.{Candidates, HanalmotToken, Prediction}
 import net.ingtra.hanalmot.util.DictionaryUtil
 
 private[hanalmot] object ThreeGramLayer {
+  private def nnpCandidate(letter: String) = {
+    new Candidates(Map(Seq(HanalmotToken(letter, "NNP")) -> 1.4))
+  }
+
   def apply(prediction: Prediction): Prediction = {
     val charArray = Array("<시작>", "<시작>") ++ prediction.chars ++ Array("<끝>", "<끝>")
     for (element <- charArray.dropRight(2).sliding(3).zipWithIndex) {
@@ -11,8 +15,11 @@ private[hanalmot] object ThreeGramLayer {
       val index = element._2
       if (prediction.candidatesArray(index).isMutable) {
         val count = DictionaryUtil.getLeft3gram(gram)
-        val candidates = new Candidates(count).normalizeWithLog().normalizeWithSum()
-        prediction.candidatesArray(index) += candidates * 1.5
+        if (count.isEmpty) {
+          prediction.candidatesArray(index) += nnpCandidate(gram(2).toString)
+        } else {
+          prediction.candidatesArray(index) += new Candidates(count).normalizeWithLog().normalizeWithSum() * 1.5
+        }
       }
     }
     for (element <- charArray.drop(1).dropRight(1).sliding(3).zipWithIndex) {
@@ -20,8 +27,11 @@ private[hanalmot] object ThreeGramLayer {
       val index = element._2
       if (prediction.candidatesArray(index).isMutable) {
         val count = DictionaryUtil.getCenter3gram(gram)
-        val candidates = new Candidates(count).normalizeWithLog().normalizeWithSum()
-        prediction.candidatesArray(index) += candidates
+        if (count.isEmpty) {
+          prediction.candidatesArray(index) += nnpCandidate(gram(1).toString)
+        } else {
+          prediction.candidatesArray(index) += new Candidates(count).normalizeWithLog().normalizeWithSum()
+        }
       }
     }
     for (element <- charArray.drop(2).sliding(3).zipWithIndex) {
@@ -29,8 +39,11 @@ private[hanalmot] object ThreeGramLayer {
       val index = element._2
       if (prediction.candidatesArray(index).isMutable) {
         val count = DictionaryUtil.getRight3gram(gram)
-        val candidates = new Candidates(count).normalizeWithLog().normalizeWithSum()
-        prediction.candidatesArray(index) += candidates * 1.5
+        if (count.isEmpty) {
+          prediction.candidatesArray(index) += nnpCandidate(gram(0).toString)
+        } else {
+          prediction.candidatesArray(index) += new Candidates(count).normalizeWithLog().normalizeWithSum() * 1.5
+        }
       }
     }
 
