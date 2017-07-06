cb40fe976d346abb0d6f28f512e74d863171ab67
cookieshake <cookieshake@hotmail.com>
2017-07-06 22:00:58 +0900

    remove cases occur only 1

--- src/main/scala/net/ingtra/hanalmot/util/MapUtil.scala
+++ src/main/scala/net/ingtra/hanalmot/util/MapUtil.scala
@@ -3,10 +3,14 @@ package net.ingtra.hanalmot.util
 
 private[hanalmot] object MapUtil {
   def normalizeWithLog[T](map: Map[T, Double]) = {
-    map.mapValues((score: Double) => Math.log(score + 1))
+    map.mapValues((score: Double) => Math.log(score))
   }
+
   def normalizeWithSum[T](map: Map[T, Double]) = {
     val sum = map.values.sum
-    map.mapValues((score: Double) => score / sum)
+    if (sum != 0)
+      map.mapValues((score: Double) => score / sum)
+    else
+      map
   }
 }
