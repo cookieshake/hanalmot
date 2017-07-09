f8f41b091893025ba5f7cb753189ba0ff68609ba
cookieshake <cookieshake@hotmail.com>
2017-07-09 21:41:42 +0900

    remove null tags

--- src/main/scala/net/ingtra/hanalmot/layer/PredictionToResultLayer.scala
+++ src/main/scala/net/ingtra/hanalmot/layer/PredictionToResultLayer.scala
@@ -5,6 +5,6 @@ import net.ingtra.hanalmot.component.{Prediction, Result}
 private[hanalmot] object PredictionToResultLayer {
   def apply(prediction: Prediction): Result = {
     prediction.candidatesArray.foreach(_.remainTop(1))
-    new Result(prediction.chars.reduceLeft(_ + _), prediction.candidatesArray.flatMap(_.content.keys.head))
+    new Result(prediction.chars.reduceLeft(_ + _), prediction.candidatesArray.flatMap(_.content.keys.head).filter(_.pos != "ZNO"))
   }
 }
