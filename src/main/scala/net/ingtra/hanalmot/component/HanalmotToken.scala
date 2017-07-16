b6e6ccb120ac0feaf8ac9824d9b1f1072a4db5f0
cookieshake <cookieshake@hotmail.com>
2017-07-16 20:01:15 +0900

    token repr changed

--- src/main/scala/net/ingtra/hanalmot/component/HanalmotToken.scala
+++ src/main/scala/net/ingtra/hanalmot/component/HanalmotToken.scala
@@ -1,5 +1,5 @@
 package net.ingtra.hanalmot.component
 
 case class HanalmotToken(var letter: String, var pos: String) {
-  override def toString: String = s"($letter, $pos)"
+  override def toString: String = s"($letter,$pos)"
 }
