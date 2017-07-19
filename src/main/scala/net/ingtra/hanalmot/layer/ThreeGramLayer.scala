eb3161d64815db4d0c3153464705977a7a88eeba
cookieshake <cookieshake@hotmail.com>
2017-07-19 17:47:45 +0900

    revoke NNPLayer

--- src/main/scala/net/ingtra/hanalmot/layer/ThreeGramLayer.scala
+++ src/main/scala/net/ingtra/hanalmot/layer/ThreeGramLayer.scala
@@ -4,10 +4,6 @@ import net.ingtra.hanalmot.component.{Candidates, HanalmotToken, Prediction}
 import net.ingtra.hanalmot.util.DictionaryUtil
 
 private[hanalmot] object ThreeGramLayer {
-  private def nnpCandidate(letter: String) = {
-    new Candidates(Map(Seq(HanalmotToken(letter, "NNP")) -> 0.1))
-  }
-
   def apply(prediction: Prediction): Prediction = {
     val charArray = Array("<시작>", "<시작>") ++ prediction.chars ++ Array("<끝>", "<끝>")
     for (element <- charArray.dropRight(2).sliding(3).zipWithIndex) {
@@ -15,11 +11,7 @@ private[hanalmot] object ThreeGramLayer {
       val index = element._2
       if (prediction.candidatesArray(index).isMutable) {
         val gramMap = DictionaryUtil.getLeft3gram(gram)
-        if (gramMap.isEmpty) {
-          prediction.candidatesArray(index) += nnpCandidate(element._1(2)) * 1.5
-        } else {
-          prediction.candidatesArray(index) += new Candidates(gramMap) * 1.5
-        }
+        prediction.candidatesArray(index) += new Candidates(gramMap) * 1.5
       }
     }
     for (element <- charArray.drop(1).dropRight(1).sliding(3).zipWithIndex) {
@@ -27,11 +19,7 @@ private[hanalmot] object ThreeGramLayer {
       val index = element._2
       if (prediction.candidatesArray(index).isMutable) {
         val gramMap = DictionaryUtil.getCenter3gram(gram)
-        if (gramMap.isEmpty) {
-          prediction.candidatesArray(index) += nnpCandidate(element._1(1))
-        } else {
-          prediction.candidatesArray(index) += new Candidates(gramMap)
-        }
+        prediction.candidatesArray(index) += new Candidates(gramMap)
       }
     }
     for (element <- charArray.drop(2).sliding(3).zipWithIndex) {
@@ -39,15 +27,11 @@ private[hanalmot] object ThreeGramLayer {
       val index = element._2
       if (prediction.candidatesArray(index).isMutable) {
         val gramMap = DictionaryUtil.getRight3gram(gram)
-        if (gramMap.isEmpty) {
-          prediction.candidatesArray(index) += nnpCandidate(element._1(0)) * 1.5
-        } else {
-          prediction.candidatesArray(index) += new Candidates(gramMap) * 1.5
-        }
+        prediction.candidatesArray(index) += new Candidates(gramMap) * 1.5
       }
     }
     prediction.candidatesArray.foreach(_.normalizeWithSum())
-    prediction.candidatesArray.foreach(_.remainTop(5))
+    prediction.candidatesArray.foreach(_.remainTop(3))
     prediction
   }
 }
